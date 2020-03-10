bfac63815f5e8a8875461b2fb215a091a84fffae
bschroeder <bschroeder@fresno.courts.ca.gov>
2020-03-10 08:12:51 -0700

    Changed code for server PORT

--- helloArthur.js
+++ helloArthur.js
@@ -1,10 +1,7 @@
 const http = require('http'); // Built in Node module for handling HTTP requests and responses
 
 const msg = 'Hello, my name is Arthur.  Please join me on this learning adventure.';  // Message to display on web page
-let port = process.env.PORT;//3000;  // Port number to serve web page on
-if(port == null || port == "") {
-  port = 8000;
-}
+const port = process.env.PORT || 8000;  // Port number to serve web page on
 
 // Create HTTP server and send/end response with simple text
 const server = http.createServer((req, res) => {
